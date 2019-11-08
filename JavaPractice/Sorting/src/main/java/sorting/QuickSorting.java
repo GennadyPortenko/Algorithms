f4e9014a9256d1689238b79995da3fbe1be579ea
Gennady Portenko <gportenko@gmail.com>
2019-11-08 13:43:11 +0400

    Refactor Quick sorting algorithm : remove redundant optimization.

--- JavaPractice/Sorting/src/main/java/sorting/QuickSorting.java
+++ JavaPractice/Sorting/src/main/java/sorting/QuickSorting.java
@@ -12,16 +12,10 @@ public class QuickSorting implements SortingAlgorithm {
     int k;
     for (int i = low; i < chosenPos; i++) {
       if (A[i] >= A[chosenPos]) {
-        if (i == chosenPos - 1) {
-          k = A[chosenPos];
-          A[chosenPos] = A[i];
-          A[i] = k;
-        } else {
-          k = A[i];
-          A[i] = A[chosenPos - 1];
-          A[chosenPos - 1] = A[chosenPos];
-          A[chosenPos] = k;
-        }
+        k = A[i];
+        A[i] = A[chosenPos - 1];
+        A[chosenPos - 1] = A[chosenPos];
+        A[chosenPos] = k;
         i--;
         chosenPos--;
       }
